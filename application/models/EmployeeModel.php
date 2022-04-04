c19a950d2d5bc29ba1679914d88f07e3587a2a34
kristan-gitaccount <kristanbalin@gmail.com>
2022-04-04 22:24:56 +0800

    add validations in model, display employee on qr scan

--- application/models/EmployeeModel.php
+++ application/models/EmployeeModel.php
@@ -5,9 +5,9 @@ class EmployeeModel extends CI_Model
     public function get_all_employee()
     {
         $this->db->select('e.*, jb.job_title_name')
-        ->from('employee as e')
-        ->join('job_title as jb', 'e.job_title_id = jb.job_title_id')
-        ->where('e.status', 'Active');
+            ->from('employee as e')
+            ->join('job_title as jb', 'e.job_title_id = jb.job_title_id')
+            ->where('e.status', 'Active');
         $query = $this->db->get();
 
         return $query->result();
@@ -34,16 +34,33 @@ class EmployeeModel extends CI_Model
     // Create
     public function create_employee($data)
     {
- 
+        $query_name = $this->db->where('first_name', $data['first_name'])
+            ->where('last_name', $data['last_name'])
+            ->where('middle_name', $data['middle_name'])
+            ->get('employee');
 
-        $this->db->where('first_name', $data['first_name'])
-        ->where('last_name', $data['last_name'])
-        ->where('middle_name', $data['middle_name']);
-        $query = $this->db->get('employee');
 
-        if ($query->num_rows() > 0) {
+        $query_email = $this->db->where('email', $data['email'])
+            ->get('employee');
+
+        $query_cellphone_no = $this->db->where('cellphone_no', $data['cellphone_no'])
+            ->get('employee');
+
+        if ($query_name->num_rows() > 0) {
             return array(
                 'message' => "Employee is already exist",
+                'status_code' => 404
+            );
+        }
+        if ($query_email->num_rows() > 0) {
+            return array(
+                'message' => "Email is already exist",
+                'status_code' => 404
+            );
+        }
+        if ($query_cellphone_no->num_rows() > 0) {
+            return array(
+                'message' => "Cellphone number is already exist",
                 'status_code' => 404
             );
         } else {
@@ -61,22 +78,22 @@ class EmployeeModel extends CI_Model
 
 
     // Update
-    public function update_employee($id,$data)
+    public function update_employee($id, $data)
     {
         $query_name = $this->db->where('first_name', $data['first_name'])
-        ->where('last_name', $data['last_name'])
-        ->where('middle_name', $data['middle_name'])
-        ->where('employee_id !=', $id)
-        ->get('employee');
+            ->where('last_name', $data['last_name'])
+            ->where('middle_name', $data['middle_name'])
+            ->where('employee_id !=', $id)
+            ->get('employee');
 
 
         $query_email = $this->db->where('email', $data['email'])
-        ->where('employee_id !=', $id)
-        ->get('employee');
+            ->where('employee_id !=', $id)
+            ->get('employee');
 
         $query_cellphone_no = $this->db->where('cellphone_no', $data['cellphone_no'])
-        ->where('employee_id !=', $id)
-        ->get('employee');
+            ->where('employee_id !=', $id)
+            ->get('employee');
 
 
         if ($query_name->num_rows() > 0) {
@@ -85,13 +102,13 @@ class EmployeeModel extends CI_Model
                 'status_code' => 404
             );
         }
-        if($query_email->num_rows() > 0){
+        if ($query_email->num_rows() > 0) {
             return array(
                 'message' => "Email is already exist",
                 'status_code' => 404
             );
         }
-        if($query_cellphone_no->num_rows() > 0){
+        if ($query_cellphone_no->num_rows() > 0) {
             return array(
                 'message' => "Cellphone number is already exist",
                 'status_code' => 404
@@ -108,15 +125,13 @@ class EmployeeModel extends CI_Model
                 log_message('error', $this->db->_error_message());
             }
         }
-   
     }
 
     // Delete
-    public function delete_employee($id,$data)
+    public function delete_employee($id, $data)
     {
         $this->db->where('employee_id', $id);
         $this->db->update('employee', $data);
-
     }
 
     // Generate random employee number
@@ -124,10 +139,9 @@ class EmployeeModel extends CI_Model
     {
 
         $this->db->select('FLOOR(RAND() * 99999) AS random_num')
-        ->from('employee')
-        ->where_not_in('employee_no','random_num');
+            ->from('employee')
+            ->where_not_in('employee_no', 'random_num');
         $query = $this->db->get();
         return $query->row_array();
-
     }
 }
